568cfe027d4477176b4ab728beb2cb53eb05df9c
rafael.soliveira <rafaelsantosoli@gmail.com>
2026-01-16 09:36:38 -0300

    docs: Remove the improvement plan document and the VPN host PowerShell script.

--- script vpn - host.ps1
+++
@@ -1,3 +0,0 @@
-Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} | Set-NetIPInterface -InterfaceMetric 4000
-
-Get-NetIPInterface -InterfaceAlias "vEthernet (FSE HostVnic)" | Set-NetIPInterface -InterfaceMetric 1
